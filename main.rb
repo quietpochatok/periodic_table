d95a227c14c103f24c91025a44afd564fee18e1e
quietpochatok <bosroin@gmail.com>
2021-01-18 23:46:37 +0500

    delete unless code for windows os

--- main.rb
+++ main.rb
@@ -1,12 +1,3 @@
-if Gem.win_platform?
-  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
-  Encoding.default_internal = __ENCODING__
-
-  [STDIN, STDOUT].each do |io|
-    io.set_encoding(Encoding.default_external, Encoding.default_internal)
-  end
-end
-
 require 'json'
 
 read_file = File.read("#{__dir__}/elements.json", encoding: 'utf-8')
